4780b23eba2c291df0dbb749f99f106c1fe3e0ea
Rushimanikanata Lingam <158359799+rushimani2@users.noreply.github.com>
2024-10-27 14:46:07 +0530

    Update additon.py

--- src/additon.py
+++ src/additon.py
@@ -19,5 +19,4 @@ while roll_again == "yes" or roll_again == "y":
     #generating and printing 2nd random integer from 1 to 6
     print(random.randint(min_val, max_val))
     
-    #asking user to roll the dice again. Any input other than yes or y will terminate the loop
-    roll_again = input("Roll the Dices Again?") 
+
